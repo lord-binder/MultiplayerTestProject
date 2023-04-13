058aba3d20e59dc2b8c8b003c23bac2ac45574f6
lor1dok <47904961+lor1dok@users.noreply.github.com>
2023-04-13 23:10:31 +0300

    make Projectile only Destroy on Player or Obstacle hit

--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class ProjectileController : MonoBehaviour {
 
-    private static string PLAYER_TAG = "Player"; 
+    private static string TAG_PLAYER = "Player";
+    private static string TAG_OBSTACLE = "Player";
+
     private float projectileSpeed = 20.0f;
     private Vector3 shootDirection;
 
@@ -19,11 +21,14 @@ public class ProjectileController : MonoBehaviour {
 
         Transform objectHit = Physics2D.CircleCast(transform.position, 0.5f, shootDirection, moveDistance).transform;
         if (objectHit != null) {
-            if(objectHit.CompareTag(PLAYER_TAG)) {
+            if (objectHit.CompareTag(TAG_PLAYER)) {
                 objectHit.GetComponent<PlayerController>().TakeDamage(1);
                 Debug.Log("Player has been damaged");
+                Destroy(gameObject);
             }
-            Destroy(gameObject);
+            else if (objectHit.CompareTag(TAG_OBSTACLE)) {
+                Destroy(gameObject);
+            }
         }
     }
 }
