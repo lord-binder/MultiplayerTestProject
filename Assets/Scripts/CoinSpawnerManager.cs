976ab158959af3fdcc318d0e53e8827c1a55b1cd
lor1dok <47904961+lor1dok@users.noreply.github.com>
2023-04-18 19:46:40 +0300

    make coin spawn on timer

--- Assets/Scripts/CoinSpawnerManager.cs
+++ Assets/Scripts/CoinSpawnerManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class CoinSpawnerManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class CoinSpawnerManager : MonoBehaviour {
+
+    [SerializeField] private GameObject coinPrefab;
+
+    [SerializeField] private float coinTimerMax = 1f;
+    private float coinTimer;
+
+    private void Start() {
+        coinTimer = coinTimerMax;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        if(coinTimer < 0) {
+            coinTimer = coinTimerMax;
+            Debug.Log("Timer is out! Spawning coin!");
+            Vector2 spawnPosition = new Vector2((Random.value * 10 - 5) * 2 * 0.9f, (Random.value * 10 - 5) * 2 * 0.9f);
+            Instantiate(coinPrefab, spawnPosition, Quaternion.identity);
+        }
+
+        coinTimer -= Time.deltaTime;
     }
 }
+
+
